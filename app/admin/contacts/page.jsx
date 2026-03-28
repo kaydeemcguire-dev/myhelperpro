2754691d8234344a1d017ca56f0e8524aaccb2c6
Kaydee McGuire <kaydeemcguire@gmail.com>
2026-03-27 20:31:19 -0700

    Fix empty contacts page

---
+++ app/admin/contacts/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function AdminContactsPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Contacts Page</h1>
+      <p>This page is ready. You can build it later.</p>
+    </div>
+  );
+}
